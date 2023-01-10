a5d9cdf57f55a4d78d111a8b0332f66cb2b26745
“Alexbarile” <alexbarile93fg@yahoo.it>
2023-01-10 16:27:21 +0100

    first commit

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    data(){
+        return{
+            Hello: "La mia prima app con Vite!"
+        }
+    },
+}
+</script>
+
+<template>
+    <h1>
+        {{ Hello }}
+    </h1>
+</template>
+
+<style>
+</style>
